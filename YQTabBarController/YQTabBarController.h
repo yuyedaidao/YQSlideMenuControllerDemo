95aa47e0056c359c7acbf658908e044cd5aab568
yuyedaidao <wyqpadding@gmail.com>
2015-06-27 17:26:11 +0800

    准备完成tabBarVC

---
+++ YQTabBarController/YQTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  YQTabBarController.h
+//  YQSlideMenuControllerDemo
+//
+//  Created by Wang on 15/6/27.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YQTabBarController : UIViewController
+
+@end
